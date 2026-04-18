b8f7bf5ae370a8114f2d5955b7a85befebfe65b8
yeyeye613 <1393250881@qq.com>
2026-04-19 02:27:10 +0800

    chore: 添加vueDevTools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-
+import { createHtmlPlugin } from "vite-plugin-html";
+import vueDevTools from "vite-plugin-vue-devtools";
 export default defineConfig({
   base: "/Arknights-alliance-tool/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // register vueDevTools before createHtmlPlugin
+    vueDevTools(),
+    createHtmlPlugin({}),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
