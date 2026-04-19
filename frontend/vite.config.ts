498eea51296df341e4d119437037c81c350ffb8a
yeyeye613 <1393250881@qq.com>
2026-04-19 16:29:12 +0800

    fix: 误添加vue devtools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { createHtmlPlugin } from "vite-plugin-html";
-import vueDevTools from "vite-plugin-vue-devtools";
 export default defineConfig({
   base: "/Arknights-alliance-tool/",
   plugins: [
     vue(),
-    // register vueDevTools before createHtmlPlugin
-    vueDevTools(),
     createHtmlPlugin({}),
   ],
   resolve: {
